d0acecf373ce1d71b220284e82133734751be837
GedeonTS <gedeontshobohwa1@gmail.com>
2022-05-26 10:42:15 +0200

    Implement the event handlers

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -3,77 +3,97 @@ import calculate from "../logic/calculate";
 import operate from "../logic/operate";
 
 class Calculator extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { total: null, next: null, operation: null };
+  }
+
+
   render() {
+    const updateObject = (e) => {
+      const myObj = calculate(this.state, e.target.textContent);
+      this.setState(myObj);
+    };
+
+    const { next, operation, total } = this.state;
+    const op = operation === '%' ? 'mod' : operation;
+    let result = '';
+    if (total) {
+      result = `${total} ${op || ''} ${next || ''}`;
+    } else if (next) {
+      result = `${next} ${op || ''}`;
+    }
+
     return (
       <div className="container">
         <div className="input-field">
-          <p> 765758</p>
+          <p> {result || 0}</p>
         </div>
         <div className="wrapper">
           <div className="row">
-            <button type="button" className="clear-small-button">
+            <button type="button" onClick={updateObject} className="clear-small-button">
               AC
             </button>
-            <button type="button" className="clear-small-button">
+            <button type="button" onClick={updateObject} className="clear-small-button">
               +/-
             </button>
-            <button type="button" className="clear-small-button">
+            <button type="button" onClick={updateObject} className="clear-small-button">
               %
             </button>
-            <button type="button" className="clear-small-button operator-button">
+            <button type="button" onClick={updateObject} className="clear-small-button operator-button">
             ÷
             </button>
           </div>
           <div className="row">
-            <button type="button" className="clear-small-button">
+            <button type="button" onClick={updateObject} className="clear-small-button">
               7
             </button>
-            <button type="button" className="clear-small-button">
+            <button type="button" onClick={updateObject} className="clear-small-button">
               8
             </button>
-            <button type="button" className="clear-small-button">
+            <button type="button" onClick={updateObject} className="clear-small-button">
               9
             </button>
-            <button type="button" className="clear-small-button operator-button">
+            <button type="button" onClick={updateObject} className="clear-small-button operator-button">
               x
             </button>
           </div>
           <div className="row">
-            <button type="button" className="clear-small-button">
+            <button type="button" onClick={updateObject} className="clear-small-button">
               4
             </button>
-            <button type="button" className="clear-small-button">
+            <button type="button" onClick={updateObject} className="clear-small-button">
               5
             </button>
-            <button type="button" className="clear-small-button">
+            <button type="button" onClick={updateObject} className="clear-small-button">
               6
             </button>
-            <button type="button" className="clear-small-button operator-button">
+            <button type="button" onClick={updateObject} className="clear-small-button operator-button">
               -
             </button>
           </div>
           <div className="row">
-            <button type="button" className="clear-small-button">
+            <button type="button" onClick={updateObject} className="clear-small-button">
               1
             </button>
-            <button type="button" className="clear-small-button">
+            <button type="button" onClick={updateObject} className="clear-small-button">
               2
             </button>
-            <button type="button" className="clear-small-button">
+            <button type="button" onClick={updateObject} className="clear-small-button">
               3
             </button>
-            <button type="button" className="clear-small-button operator-button">
-              -
+            <button type="button" onClick={updateObject} className="clear-small-button operator-button">
+              +
             </button>
           </div>
           <div className="row">
-            <button type="button" className="clear-large-button">
+            <button type="button" onClick={updateObject} className="clear-large-button">
               0
             </button>
-            <button type="button" className="clear-small-button">
+            <button type="button" onClick={updateObject} className="clear-small-button">
               .
             </button>
-            <button type="button" className="clear-small-button operator-button">
+            <button type="button" onClick={updateObject} className="clear-small-button operator-button">
               =
             </button>
           </div>
