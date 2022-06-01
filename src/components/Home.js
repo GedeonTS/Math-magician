78dab88cce32a1dedbf2c2017cb1aa21f8cafe10
GedeonTS <gedeontshobohwa1@gmail.com>
2022-06-01 13:57:44 +0200

    add header to the element

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from './Header';
+
+function Home() {
+    return (
+        <>
+            <Header/>
+            <h2>Look!, this is our HOME page!</h2>
+        </>
+    )
+}
+
+export default Home
