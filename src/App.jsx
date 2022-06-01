7446657d0e469d200a42922ea3941cc394f3c446
GedeonTS <gedeontshobohwa1@gmail.com>
2022-06-01 13:59:37 +0200

    add links to the header

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
 import React, { Component } from "react";
+import { Routes, Route } from "react-router-dom";
 import Calculator from "./components/Calculator";
+import Home from "./components/Home";
+import Quote from "./components/Quote";
+
 
 class App extends Component {
   render() {
-    return <Calculator />;
+    return (
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="Quote" element={<Quote />} />
+          <Route path="Calculator" element={<Calculator />} />
+        </Routes>
+    );
   }
 }
 
