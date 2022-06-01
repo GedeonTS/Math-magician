e8226b5da68d16fcd5e134f18f0979de61674678
GedeonTS <gedeontshobohwa1@gmail.com>
2022-06-01 13:58:31 +0200

    add header to the home page

---
+++ src/components/Quote.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from './Header';
+
+function Quote() {
+  return (
+    <>
+      <Header/>
+      <p>
+          That feels like an existential question, don't you
+          think?
+      </p>
+    </>
+  )
+}
+
+export default Quote
