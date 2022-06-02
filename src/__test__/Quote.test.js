7718c5f60214b4c4c22ede456cf68249e0ecebf6
GedeonTS <gedeontshobohwa1@gmail.com>
2022-06-02 17:32:48 +0200

    create test for componnents

---
+++ src/__test__/Quote.test.js
@@ -0,0 +1,13 @@
+import React from "react";
+import  renderer from 'react-test-renderer';
+import Quote from '../components/Quote';
+import '@testing-library/jest-dom';
+
+describe('Test of Quote page', () => {
+    it('test if Quote page is rendered', () => {
+        const quoteTest = renderer
+            .create(<Quote />)
+            .toJSON();
+        expect(quoteTest).toMatchSnapshot();
+    })
+})
--- src/components/Quote.js
+++ src/components/Quote.js
@@ -4,7 +4,6 @@ function Quote() {
   return (
     <section className="quote-section">
       <p>
-        {' '}
         Mathmatics is not about numbers,eauations, computations,
         or algorithms: it is about understanding.
         <span className="author">-William Paul Thurston</span>
