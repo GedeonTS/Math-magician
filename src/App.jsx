a95edb14c20338ca0f575e57b5908d6ec00c17fe
GedeonTS <gedeontshobohwa1@gmail.com>
2022-06-02 15:52:02 +0200

    add header to the page

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,20 @@ import { Routes, Route } from "react-router-dom";
 import Calculator from "./components/Calculator";
 import Home from "./components/Home";
 import Quote from "./components/Quote";
+import Header from "./components/Header";
 
 
 class App extends Component {
   render() {
     return (
+      <>
+      <Header/>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="Quote" element={<Quote />} />
           <Route path="Calculator" element={<Calculator />} />
-        </Routes>
+      </Routes>
+      </>
     );
   }
 }
